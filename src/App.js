2a492afb7d9f278248662b0a3b006713cea42f02
Yuliia Skabitskaya <yulik777@i.ua>
2022-04-17 14:14:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';  
+import { Pie } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-export default App;
+
+export const data = {
+  labels: ['Audi', 'BMW', 'Volkswagen', 'Hyundai', 'Renault', 'Bentley'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [15, 18,9, 11, 8, 5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+
+ function App() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <h1>
+           World statistic
+         </h1>
+         <div>Most popular car brends</div>
+       </header>
+       <main className="App-chart">
+         <Pie data={data} />
+       </main>
+     </div>
+   );
+ }
+
+ export default App;
